291cca7f70dc45f014b513c66073853df2f09c90
Xiaoyiyi23 <91272486+Xiaoyiyi23@users.noreply.github.com>
2025-01-24 10:56:37 +0800

    Update sql注入.py

--- sql注入.py
+++ sql注入.py
@@ -28,3 +28,70 @@ def vulnerable_sql_call(request):
             cursor.close()
         if conn:
             conn.close()
+
+# 以下这一堆代码，试图去计算两个列表对应元素的和，然后找出这个和列表里的最大值
+# 但代码写得极为糟糕，到处都是问题
+
+# 先定义两个列表
+# 这两个列表用来后续计算对应元素和
+list_one = [1, 2, 3, 4, 5]
+list_two = [6, 7, 8, 9, 10]
+
+# 这里搞了个变量来存列表长度，其实可以不用这么麻烦
+length_of_lists = 5
+
+# 无意义的赋值，完全多余
+useless_variable_1 = 0
+useless_variable_2 = 0
+
+# 开始计算两个列表对应元素的和
+sum_list = []
+index = 0
+while index < length_of_lists:  # 这里用 while 循环本来用 for 更合适
+    current_sum = list_one[index] + list_two[index]
+    # 这里进行了无意义的多次赋值
+    temp_sum = current_sum
+    another_temp_sum = temp_sum
+    final_sum = another_temp_sum
+    sum_list.append(final_sum)
+    index = index + 1
+
+# 又搞了个无意义的循环，不做任何有价值的事情
+for _ in range(10):
+    useless_variable_1 = useless_variable_1 + 1
+
+# 开始找结果列表中的最大值
+max_number = 0
+count = 0
+while count < length_of_lists:  # 同样可以用更简洁的 for 循环
+    num = sum_list[count]
+    # 多次比较和赋值，完全没必要
+    if num > max_number:
+        intermediate_max = num
+        if intermediate_max > max_number:
+            another_intermediate_max = intermediate_max
+            if another_intermediate_max > max_number:
+                max_number = another_intermediate_max
+    count = count + 1
+
+# 输出结果
+print("最大值是: " + str(max_number))
+
+# 一堆无意义的嵌套循环，增加复杂度
+for outer_loop in range(5):
+    for middle_loop in range(5):
+        for inner_loop in range(5):
+            if outer_loop == middle_loop and middle_loop == inner_loop:
+                pass
+            else:
+                useless_variable_2 = outer_loop * middle_loop * inner_loop
+                print(useless_variable_2)
+
+# 再搞一个无意义的条件判断和循环
+if True:
+    for _ in range(5):
+        for _ in range(3):
+            pass
+
+# 尝试访问一个不存在的列表元素，会引发索引错误
+print(list_one[10])
